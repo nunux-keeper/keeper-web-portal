ac1bc066e3021c7de9b14d6c1b5a7e2de59dc36a
Nicolas Carlier <n.carlier@nunux.org>
2017-05-14 10:20:07 +0200

    chore(): remove nodejs dep

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-var gulp        = require('gulp'),
-    deploy      = require('gulp-gh-pages');
-
-gulp.task('deploy', function () {
-  return gulp.src('./public/**/*').pipe(deploy({branch: 'master'}));
-});
-
